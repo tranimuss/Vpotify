aab43ae8d7afb23a69d09565fdf673d2bc44d636
tranimuss <petya.snos@gmail.com>
2020-08-07 15:17:45 +0300

    Main Files
    Vpotify.py - main script
    stoptransfer.py - Spotify class
    Vpotify.rar - Compressed executable with the needed webdriver

---
+++ Vpotify.py
@@ -0,0 +1,118 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import pickle
+import spottransfer
+from sys import exit
+import PySimpleGUI as sg
+
+def save_obj(obj, name):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+def spotify_transfer(username): #"qszc84oktwgj54ui831wfgvwz"
+	clientid = "e6b6ffba9aa741bb80d007785f2162e5"
+	clientsecret = "ccceb9144379499082630c0df67f6888"
+	redirect = 'http://localhost:7777/callback'
+	scope = 'playlist-modify-private'
+	spotify = spottransfer.Spotify(username, clientid, clientsecret, redirect, scope)
+	spotify.get_userid()
+	playlist_id = spotify.create_playlist("VK dump", "These are my songs from VK")
+	try:
+		songs = load_obj("D:/botpy/parse/songs")
+	except:
+		print("ERROR: songs file not found")
+
+	while True:
+		try:
+			song = songs.popitem()
+		except:
+			break
+		song_id = spotify.search_song(song[0] + " " + song[1])
+		if song_id != None:
+			spotify.add_song_to_playlist(playlist_id, song_id)
+
+sg.theme('DarkAmber')
+layout = [  [sg.Text('VK Username'), sg.InputText()], 
+			[sg.Text('Vk Password'), sg.InputText()], 
+			[sg.Text('Spotify Username'), sg.InputText()],
+			[sg.Button('Ok'), sg.Button('Cancel')] 
+		]
+
+window = sg.Window('Vpotify', layout)
+
+while True:
+	event, values = window.read()
+	if event == sg.WIN_CLOSED or event == 'Cancel':
+		break
+	if event == 'Ok':
+		print(values)
+		break
+
+window.close()
+
+options = webdriver.ChromeOptions() 
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+try:
+	driver = webdriver.Chrome(options=options, executable_path="chromedriver.exe")
+	driver.get("https://vk.com/")
+	assert "ВКонтакте" in driver.title
+	print("Opened VK!")
+except:
+	print("Something wrong happened!")
+	exit(1)
+
+username = values[0]
+username_field = driver.find_element_by_id('index_email')
+username_field.clear()
+username_field.send_keys(username)
+
+password = values[1]
+password_field = driver.find_element_by_id('index_pass')
+password_field.clear()
+password_field.send_keys(password)
+password_field.send_keys(Keys.ENTER)
+print("Logged in!")
+
+print("Waiting to load...")
+sleep(2)
+driver.find_element_by_id("l_aud").click() 
+sleep(2)
+print("Opened audios!")
+print("Getting titles...")
+names_elem = driver.find_elements_by_class_name("audio_row__title_inner")
+artists_elem = driver.find_elements_by_class_name("audio_row__performers")
+names = []
+artists = []
+for name in names_elem:
+	names.append(name.get_attribute('innerHTML'))
+for artist in artists_elem:
+	html = BeautifulSoup(artist.get_attribute('innerHTML'), features="lxml")
+	artists.append(html.get_text())
+songs = {}
+for name in names:
+	songs.update({name:artists[names.index(name)]})
+print("Found ", len(songs), " songs")
+save_obj(songs, "songs")
+driver.close()
+print("Songs file was created in the script repository")
+spotify_transfer(values[2])
+print("Finished! All available songs were added to your private Spotify playlist")
+
+layout = [  [], 
+			[sg.Text('Finished! All available songs were added to your private Spotify playlist')],
+			[sg.Button('Ok')],
+			[] 
+		]
+
+window = sg.Window('Vpotify', layout)
+
+while True:
+	event, values = window.read()
+	if event == sg.WIN_CLOSED or event == 'Ok':
+		break
---
+++ spottransfer.py
@@ -0,0 +1,105 @@
+import json
+import requests
+import spotipy.util as util
+import pickle
+
+def load_obj(name):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+class Spotify(object):
+	def __init__(self, username, client_id, client_secret, redirect_uri, scope):
+		self.username = username  #'your-spotify-username'
+		self.client_id = client_id #'your-client-id'
+		self.client_secret = client_secret #'your-client-secret'
+		self.redirect_uri = redirect_uri #'http://localhost:7777/callback'
+		self.scope = scope #'playlist-modify-private'
+		self.token = util.prompt_for_user_token(username=self.username, scope=self.scope, client_id=self.client_id, client_secret=self.client_secret, redirect_uri=self.redirect_uri)
+		print("Spotify initialised!")
+
+	def get_userid(self):
+		query = "https://api.spotify.com/v1/me"
+		response = requests.get(
+			query,
+			headers = {
+				"Authorization": "Bearer {}".format(self.token)
+			}
+		)
+		response_json = response.json()
+		self.user_id = response_json["id"]
+		print("Got a user id!")
+		return(response_json["id"])
+
+	def create_playlist(self, name, description):
+		request_body = json.dumps({
+			"name":name,
+			"description":description,
+			"public": False
+		})
+
+		query = "https://api.spotify.com/v1/users/{}/playlists".format(self.user_id)
+		response = requests.post(
+			query,
+			data = request_body,
+			headers = {
+				"Authorization":"Bearer {}".format(self.token),
+				"Content-Type":"application/json"
+			}
+		)
+		response_json = response.json()
+		print("Created a new playlist named", name)
+		return(response_json["id"])
+
+	def search_song(self, q, type = "track", limit = 1):
+		query = "https://api.spotify.com/v1/search?q=" + q.replace(" ", "%20") + "&type=" + type + "&limit=" + str(limit)
+		response = requests.get(
+			query,
+			headers = {
+				"Authorization":"Bearer {}".format(self.token),
+			}
+		)
+		try:
+			response_json = response.json()
+			response_json = response_json.popitem()
+			response_json = response_json[1].get('items')
+			response_json = response_json[0]
+			songid = response_json['id']
+			return(songid)
+		except:
+			print("Wasn't able to find ", q)
+			return(None)
+
+	def add_song_to_playlist(self, playlist_id, song_id):
+		query = "https://api.spotify.com/v1/playlists/{}/tracks".format(playlist_id)
+		query = query + "?uris=spotify:track:" + song_id + "&position=0"
+		response = requests.post(
+			query,
+			headers = {
+				"Authorization":"Bearer {}".format(self.token),
+				"Content-Type":"application/json"
+			}
+		)
+		response_json = response.json()
+		print("Added", song_id, "to the playlist")
+
+# username = input("Input Spotify username (i.e. qszc84surgfj54ui831wfgvw):\n")#"qszc84oktwgj54ui831wfgvwz"
+# clientid = "e6b6ffba9aa741bb80d007785f2162e5"
+# clientsecret = "ccceb9144379499082630c0df67f6888"
+# redirect = 'http://localhost:7777/callback'
+# scope = 'playlist-modify-private'
+# spotify = Spotify(username, clientid, clientsecret, redirect, scope)
+# spotify.get_userid()
+# playlist_id = spotify.create_playlist("VK dump", "These are my songs from VK")
+# try:
+# 	songs = load_obj("D:/botpy/parse/songs")
+# except:
+# 	print("ERROR: songs file not found")
+
+# while True:
+# 	try:
+# 		song = songs.popitem()
+# 	except:
+# 		break
+# 	song_id = spotify.search_song(song[0] + " " + song[1])
+# 	if song_id != None:
+# 		spotify.add_song_to_playlist(playlist_id, song_id)
